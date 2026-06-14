f82ef075b15ee4433a2c24644d3d897b92671a3a
TyaVa5 <savelyi-tyavin2009@yandex.ru>
2026-06-14 21:33:53 +0300

    Тест сам отправить в гит

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('hello')
+    print('hi')
 
 
 # Press the green button in the gutter to run the script.
@@ -15,4 +16,3 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-g
