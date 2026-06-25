5798a82f9931ee7f486b3bc70a664751845c6b67
TyaVa5 <savelyi-tyavin2009@yandex.ru>
2026-06-25 20:53:31 +0300

    5.6 Работа с авторизацией в Playwright, Практика работы с авторизацией в Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,32 +1,12 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import sync_playwright
+
+# Остальной код регистрации нового пользователя без изменений
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    context = browser.new_context(storage_state="browser-state.json")  # Указываем файл с сохраненным состоянием
+    page = context.new_page()
 
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
 
-    registration_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    expect(registration_email_input).to_be_visible()
-    registration_email_input.fill("user.name@gmail.com")
-    page.wait_for_timeout(1000)
-
-    registration_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    expect(registration_username_input).to_be_visible()
-    registration_username_input.fill("username")
-    page.wait_for_timeout(1000)
-
-    registration_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    expect(registration_password_input).to_be_visible()
-    registration_password_input.fill("password")
-    page.wait_for_timeout(1000)
-
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    expect(registration_button).to_be_visible()
-    registration_button.click()
-
-    header_dashboard = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(header_dashboard).to_be_visible()
-    expect(header_dashboard).to_have_text("Dashboard")
-
-    page.wait_for_timeout(2000)
+    page.wait_for_timeout(5000)
