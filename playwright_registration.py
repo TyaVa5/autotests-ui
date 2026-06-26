a9b2366744f1a599e766143ca79249a925e881f9
TyaVa5 <savelyi-tyavin2009@yandex.ru>
2026-06-26 17:42:09 +0300

    6.2 Интеграция Pytest и Playwright, Практика написания автотестов

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,6 +1,27 @@
 from playwright.sync_api import sync_playwright
 
-# Остальной код регистрации нового пользователя без изменений
+with sync_playwright() as playwright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()  # Создание контекста
+    page = context.new_page()  # Создание страницы
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+    page.wait_for_timeout(1000)
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+    page.wait_for_timeout(1000)
+
+    context.storage_state(path="browser-state.json")
 
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
