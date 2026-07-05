9a4605e211a7cada60db29f970dde5f6be512243
TyaVa5 <savelyi-tyavin2009@yandex.ru>
2026-07-05 21:18:33 +0300

    replace for problems

--- tests/conftest.py
+++ tests/conftest.py
@@ -48,8 +48,6 @@ def initialize_browser_state(playwright: Playwright):
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
-    page = context.new_page()
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-    yield page
+    yield context.new_page()
     browser.close()
 
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -4,8 +4,8 @@ from playwright.sync_api import Playwright, Page, expect
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list(chromium_page_with_state):
-
+def test_empty_courses_list(chromium_page_with_state :Page):
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
     header_courses = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
     expect(header_courses).to_be_enabled()
     expect(header_courses).to_have_text("Courses")
