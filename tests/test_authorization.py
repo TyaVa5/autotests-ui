1d96f13b4df15b9d0240bcfcb234071b811ff65c
TyaVa5 <savelyi-tyavin2009@yandex.ru>
2026-07-05 23:13:39 +0300

    6.6 Плагины Pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -20,6 +20,6 @@ def test_wrong_email_or_password_authorization(chromium_page: Page):
         wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
         expect(wrong_email_or_password_alert).to_be_visible()
         expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
-
+        chromium_page.wait_for_timeout(500)
 
 
