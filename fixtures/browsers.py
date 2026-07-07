75ef7b2af46e5d8c496575a6773e2827a004539c
TyaVa5 <savelyi-tyavin2009@yandex.ru>
2026-07-07 10:02:12 +0300

    test

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -44,7 +44,7 @@ def initialize_browser_state(playwright: Playwright):
     context.storage_state(path="browser-state.json")
     browser.close()
 
-@pytest.fixture(scope="function")
+@pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
