fb5acb9c0b6925d6ea366c607fddaef574384e1d
TyaVa5 <savelyi-tyavin2009@yandex.ru>
2026-07-29 20:19:12 +0300

    8.2 Интеграция Allure в автотесты

--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -1,13 +1,27 @@
 import pytest
+import allure
+from allure_commons.types import Severity
+
 from pages.dashboard.dashboard_page import DashboardPage
 from pages.authentication.registration_page import RegistrationPage
-
+from tools.allure.tags import AllureTag
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.epics import AllureEpic
 
 @pytest.mark.regression
 @pytest.mark.registration
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.REGISTRATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.REGISTRATION)
+@allure.tag(AllureTag.REGRESSION, AllureTag.REGISTRATION)
 class TestRegistration:
 
-
+    @allure.title("Registration with correct email, username and password")
+    @allure.severity(Severity.CRITICAL)
     def test_successful_registration(self, registration_page: RegistrationPage, dashboard_page: DashboardPage):
         registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
         registration_page.registration_form.check_visible(email = "", password = "", username = "")
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,12 +1,25 @@
 import pytest
-
+import allure
+from allure_commons.types import Severity
 from pages.courses.courses_list_page import CoursesListPage
 from pages.courses.create_course_page import CreateCoursePage
-
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic
+from tools.allure.stories import AllureStory
+from tools.allure.features import AllureFeature
 
 @pytest.mark.courses
 @pytest.mark.regression
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.COURSES)
+@allure.story(AllureStory.COURSES)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.COURSES)
+@allure.sub_suite(AllureStory.COURSES)
+@allure.tag(AllureTag.COURSES, AllureTag.REGRESSION)
 class TestCourses:
+    @allure.title("Check displaying of empty courses list")
+    @allure.severity(Severity.NORMAL)
     def test_empty_courses_list(self, courses_list_page: CoursesListPage):
         courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
         courses_list_page.sidebar.check_visible()
@@ -14,7 +27,8 @@ class TestCourses:
         courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
 
-
+    @allure.title("Create course")
+    @allure.severity(Severity.CRITICAL)
     def test_create_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
         create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
         create_course_page.create_course_toolbar_view_component.check_visible(is_create_course_disabled=True)
@@ -47,6 +61,8 @@ class TestCourses:
             estimated_time="2 weeks"
         )
 
+    @allure.title("Edit course")
+    @allure.severity(Severity.CRITICAL)
     def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
         create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
         create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
